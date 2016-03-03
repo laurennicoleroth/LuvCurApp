b42ffd2fc8ee22ff08449652d07e0b6aaf83af81
Lauren Nicole Roth <laurenroth@gmail.com>
2016-03-03 15:59:26 -0500

    Add scene and assets

---
+++ LuvCurApp/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  LuvCurApp
+//
+//  Created by Lauren Nicole Roth on 3/3/16.
+//  Copyright © 2016 LuvCurApp. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController
+{
+
+
+}
+
